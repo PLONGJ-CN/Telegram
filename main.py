eddc5b10f8119bad0781265f0d9820d084d14ed4
jiangpenglong <jiangpenglong@huafagroup.com>
2025-12-23 15:09:33 +0800

    提交代码

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 from telethon import TelegramClient
 
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # 加载 .env 文件中的环境变量
+
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 
